afe36fe83a70c116dc5ab095cad59812ad49bf75
Josue <josue@roho.fitness>
2019-03-28 02:03:20 -0700

    search bar

--- iOS GrainChain Test/iOS GrainChain Test/ContactsController.swift
+++ iOS GrainChain Test/iOS GrainChain Test/ContactsController.swift
@@ -8,29 +8,46 @@
 
 import UIKit
 
-class ContactsController: UIViewController, UITableViewDelegate,UITableViewDataSource {
-
+class ContactsController: UIViewController, UITableViewDelegate,UITableViewDataSource, UISearchControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating {
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var navBarContacts: UINavigationBar!
     
+    var isFiltering = false
+    var filterArray = [String:[String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering{
+            return filterArray.count
+        }
         return ContactsInfo.instance.arrayName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsTableViewCell") as! ContactsTableViewCell
         let row = indexPath.row
+        isFiltering ? infoContactsFiltering(cell, row) : infoContacts(cell, row)
+        return cell
+    }
+    
+    func infoContactsFiltering(_ cell: ContactsTableViewCell, _ row: Int){
+        var pathName = (filterArray.keys.map{value in value})
+        cell.txtName.text! = pathName[row]
+        cell.txtLastName.text! = filterArray[pathName[row]]![0]
+        cell.txtAge.text! = "\(filterArray[pathName[row]]![1])"
+        cell.txtPhone.text! = filterArray[pathName[row]]![2]
+    }
+    
+    func infoContacts(_ cell: ContactsTableViewCell, _ row: Int){
         cell.txtName.text! = ContactsInfo.instance.arrayName[row]
         cell.txtLastName.text! = ContactsInfo.instance.arrayLastName[row]
         cell.txtAge.text! = "\(ContactsInfo.instance.arrayAge[row])"
         cell.txtPhone.text! = ContactsInfo.instance.arrayPhone[row]
-        return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -47,5 +64,38 @@ class ContactsController: UIViewController, UITableViewDelegate,UITableViewDataS
         ContactsInfo.instance.arrayAge.remove(at: row)
         ContactsInfo.instance.arrayPhone.remove(at: row)
     }
-
+    
+    @IBAction func itemSearch(_ sender: Any) {
+        ContactsInfo.instance.assignDict()
+        searchController().hidesNavigationBarDuringPresentation = false
+        searchController().searchBar.keyboardType = UIKeyboardType.asciiCapable
+        self.searchController().searchBar.delegate = self
+        present(searchController(), animated: true, completion: nil)
+    }
+    
+    func searchController()->UISearchController{
+        let controller = UISearchController(searchResultsController: nil)
+        controller.delegate = self
+        controller.searchBar.delegate = self
+        controller.searchResultsUpdater = self
+        controller.dimsBackgroundDuringPresentation = false
+        controller.hidesNavigationBarDuringPresentation = true
+        controller.searchBar.sizeToFit()
+        return controller
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if searchController.searchBar.text!.isEmpty{
+            isFiltering = false
+            tableView.reloadData()
+            return
+        }
+         filterArray = ContactsInfo.instance.dictContact.filter { (contacts) -> Bool in
+            return contacts.key.lowercased().contains(searchController.searchBar.text!.lowercased())
+        }
+        isFiltering = true
+        tableView.reloadData()
+    }
+    
+   
 }
--- iOS GrainChain Test/iOS GrainChain Test/ContactsInfo.swift
+++ iOS GrainChain Test/iOS GrainChain Test/ContactsInfo.swift
@@ -11,10 +11,19 @@ import Foundation
 class ContactsInfo {
     
     static let instance = ContactsInfo()
+    var dictContact = [String:[String]]()
     
     var arrayName = ["Josue","Adrian","Tomas","Enrique","Andres","Luis","Lael","Ivan","Jose","Alejandro"]
     var arrayLastName = ["Enriquez","Villanueva","Talamantes","Sigala","Escobedo","Ayala","Hernandez","Chavira","Torres","Gomez"]
     var arrayAge = [21,14,18,56,99,34,50,41,25,29]
     var arrayPhone = ["6141957874","6141874523","6143981234","6141345687","6143996577","6141874523","6143981234","6141345687","6143996577","6142897434"]
     
+    func assignDict(){
+        for (index,path) in arrayName.enumerated() {
+            dictContact[path] = [arrayLastName[index],"\(arrayAge[index])",arrayPhone[index]]
+        }
+    }
+    
 }
+
+
